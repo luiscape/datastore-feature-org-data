297ae2f868877ed96176271414a0d6aa6ce2a704
Luis Capelo <luiscape@gmail.com>
2015-09-11 10:09:14 -0400

    changing path

--- bin/run.sh
+++ bin/run.sh
@@ -10,4 +10,4 @@ RESOURCE_ID=1f3dbfae-76d1-42da-bc29-b497fa144e90
 # Running script.
 #
 source venv/bin/activate
-python scripts/create-datastore.py $RESOURCE_ID $API_KEY
+python scripts/create-datastore-stag.py $RESOURCE_ID $API_KEY
